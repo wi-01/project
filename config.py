86c5318fe1266c722ba48527df6e5f067fad65f9
wi-01 <harunxx10@gmail.com>
2025-03-13 19:29:46 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+main_config = {
+    "Token": "TOKEN",
+    "Prefix": "$",
+}
+
+command_categories = {
+    "General": {
+        "hello": "Greets the user.",
+        "bye": "Says goodbye to the user.",
+        "random_password": "Generates a random password.",
+        "random_emoji": "Displays a random emoji.",
+        "flip_coin": "Flips a coin.",
+        "user_info": "Shows information about a user.",
+        "react": "Adds a reaction to a message.",
+        "poll": "Creates a simple poll."
+    },
+    "Moderation": {
+        "warn": "Warns a user.",
+        "warnings_list": "Displays a user's warnings.",
+        "kick": "Kicks a user.",
+        "ban": "Bans a user.",
+        "unban": "Unbans a user."
+    },
+    "Configuration": {
+        "set_log_channel": "Sets the logging channel for this server.",
+        "set_bad_words": "Configures the list of banned words for this server."
+    },
+    "Testing": {
+        "test": "Command for testing purposes."
+    },
+}
